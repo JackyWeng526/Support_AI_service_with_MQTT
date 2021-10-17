8aa20fa2f73c948b6861084b9b807dbf31f61447
JackyWeng526 <63989896+JackyWeng526@users.noreply.github.com>
2021-10-17 23:32:47 +0800

    Add files via upload

---
+++ src/mqtt_publisher_template.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+import json  
+import time
+
+# MQTT Client
+client = mqtt.Client()
+
+# connect(IP, Port, connectionTime)
+client.connect("114.32.111.46", 1883, 30)
+
+time.sleep(1)
+
+# make a test data
+payload = {"Time" : "2021-10-16T16:38:02", "Temperature" : 30.2 , "Humid": 58.0}
+
+msg = json.dumps(payload)
+print(msg)
+
+client.publish("test/test_message", msg)
---
+++ src/mqtt_subscriber_template.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+import json
+
+def on_connect(client, userdata, flags, rc):
+    print(F"Connected with result code {str(rc)}")
+    print("Waiting for messages...")
+    client.subscribe("test/test_message")
+
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload)
+    print(data)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("114.32.111.46", 1883, 30)
+client.loop_forever()
